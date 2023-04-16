4a26272c1a2bc0622ea458377d8bc887239acfd9
alikon <optimus4joomla@gmail.com>
2023-04-16 10:49:11 +0200

    usecols

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import streamlit as st
 
 
 st.title("Perceptron")
-df = pd.read_csv("dataset.csv")
+df = pd.read_csv("dataset.csv"header=0,
+        usecols=["x1", "x2", "label"])
 st.write(df.to_string())
 
 #x_train = df[["x1", "x2"]].values
