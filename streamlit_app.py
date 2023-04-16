3114d6f150275ee05c8e72afea103bc2bc737166
alikon <optimus4joomla@gmail.com>
2023-04-16 11:47:46 +0200

    plt

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,8 @@ x = [2,3,7,29,8,5,13,11,22,33]
 # Y axis values:
 y = [4,7,55,43,2,4,11,22,33,44]
 
+fig = plt.figure()
 # Create scatter plot:
 plt.scatter(x, y)
 
-st.write(plt.show())
+st.pyplot(fig)
