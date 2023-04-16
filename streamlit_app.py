a5345cdda284d1b696720b292b96cbf3653ff4c7
alikon <optimus4joomla@gmail.com>
2023-04-16 10:45:41 +0200

    df

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 st.title("Perceptron")
 df = pd.read_csv("dataset.csv")
-st.write(df.info())
+st.write(df.to_string())
 
 #x_train = df[["x1", "x2"]].values
 #y_train = df[["label"]]. values
